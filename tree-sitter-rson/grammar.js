e379a99468b5a5867bc8875c4e6a847a7fc7e2b0
Marcel Müller <neikos@neikos.email>
2025-02-12 18:20:56 +0100

    Add simple base grammar
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- tree-sitter-rson/grammar.js
+++ tree-sitter-rson/grammar.js
@@ -10,18 +10,52 @@
 module.exports = grammar({
   name: "rson",
 
+  extras: $ => [/\s/, $._comment],
+
+  conflicts: $ => [[$.map, $._fields]],
+
   rules: {
     source_file: $ => $._expression,
     _expression: $ => choice(
       $.struct,
-      $.string
+      $.map,
+      $.array,
+      $._literal
     ),
-    identifier: $ => /[a-zA-Z][a-zA-Z0-9]+/,
-    struct: $ => seq($.identifier, $._fields),
-    _fields: $ => seq('{', repeat($.field), '}'),
-    field: $ => seq($.identifier, ':', $._expression, optional(',')),
+    identifier: $ => /[_\p{XID_Start}][_\p{XID_Continue}]*/,
+    struct: $ => seq(optional($.identifier), field('body', $._fields)),
+    _fields: $ => seq('{', sepBy(',', $.field), optional(','), '}'),
+    field: $ => seq($.identifier, ':', $._expression),
+    map: $ => seq('{', sepBy(',', $.map_field), optional(','), '}'),
+    map_field: $ => seq($._expression, ':', $._expression),
     string: $ => seq('"', /[^"]*/, '"'),
-    comment: $ => token(seq('//', /.*/)),
+    char: $ => seq('\'', /[^\']/, '\''),
+    _comment: $ => token(seq('//', /.*/)),
+    array: $ => seq('[', sepBy(',', $._expression), optional(','), ']'),
+    boolean: $ => choice('true', 'false'),
+    integer: $ => token(seq(
+      choice(
+        /[0-9][0-9_]*/, // Normal nums
+        /0x[0-9a-fA-F_]+/, // hex nums
+        /0o[0-7_]+/, // octal nums
+        /0b[0-1_]+/, // binary nums
+      )
+    )),
+    float: $ => token(seq(optional(/[0-9][0-9]*/), '.', /[0-9][0-9]*/)),
+    _literal: $ => choice(
+      $.string,
+      $.char,
+      $.boolean,
+      $.integer,
+      $.float,
+    ),
   },
-  extra: $ => [/\s/, $.comment]
 });
+
+function sepBy1(sep, rule) {
+  return seq(rule, repeat(seq(sep, rule)));
+}
+
+function sepBy(sep, rule) {
+  return optional(sepBy1(sep, rule));
+}
